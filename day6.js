4bc97e932aeca439ca4ffb32cfa78c82aa42edc6
ex7z <damian.kitov@gmail.com>
2021-12-06 18:30:22 +0200

    day 6

--- day6.js
+++ day6.js
@@ -3,22 +3,53 @@ const readline = require('readline');
 
 getInput('DaysInput/Day6.txt');
 
+let inputs;
 async function getInput(_file) { 
   const fileStream = fs.createReadStream(_file);
   const readLine   = readline.createInterface({ input: fileStream });  
 
-  let arr;
+  
   for await (const line of readLine) {
-    arr = line.split(',').map(n => parseInt(n))
+    inputs = line.split(',').map(n => parseInt(n))
   }
-  for (i=0; i< 80; i++) {
-    simulation(arr)
+  
+  let count = 0
+
+  inputs.forEach(x => {
+    count += reproduceDays(x, 80)
+  })
+  console.log(count);
+  
+  let count2 = 0
+  inputs.forEach(x => {
+    count2 += reproduceDays(x, 256)
+  })
+  console.log(count2);
+
+  // simulation(arr)
+}
+
+let obj = {}
+function reproduceDays(start, days) {
+  const startDays = start.toString() + days.toString();
+  
+  if (!obj[startDays]) {
+    let count = 1;
+    let next  = days - start;
+
+    while (next > 0) {
+      count   += reproduceDays(8, next - 1);
+      next -= 7;
+    }
+    obj[startDays] = count;
   }
+
+  return obj[startDays];
 }
 
 
+
 function simulation(_arr) {
-  let turn = 0;
   let newFish = 9;
   for (let i=0; i< _arr.length; i++) {
     if (_arr[i] == 0) {
@@ -26,6 +57,6 @@ function simulation(_arr) {
       _arr[i] = 7;
     } 
     _arr[i] -= 1;
-  }
+  }  
   console.log(_arr.length)
 }

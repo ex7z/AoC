2b265b6922ae0ec2cfd374d3a6b32f481f9b901b
ex7z <damian.kitov@gmail.com>
2022-12-12 16:39:45 +0200

    gg

--- 2022/day6.js
+++ 2022/day6.js
@@ -1,29 +1,57 @@
 const fs     = require('fs'      );
-//const inputs = fs.readFileSync("./DaysInput/Day6.txt").toString();
+const inputs = fs.readFileSync("./DaysInput/Day6.txt").toString();
 
-const inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+// const inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // part1
+// const inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // part2
 
+function part1() {
+  let arr = [];
+  let isMarker = false; 
+  for (let i = 0; i< inputs.length; i++) {  
+    arr.push(inputs[i]);  
+    if (arr.length < 4) continue;
+    for (let j = 0; j< arr.length; j++) {
+      for (let k = j + 1; k< arr.length; k++) {  
+        if (arr[j] == arr[k]) {
+          arr = arr.slice(j+1);
+          break;
+        }
+      }  
 
-let arr = [];
-let isMarker = false; 
-for (let i = 0; i< inputs.length; i++) {  
-  arr.push(inputs[i]);  
-  if (arr.length < 4) continue;
-  for (let j = 0; j< arr.length; j++) {
-    for (let k = j + 1; k< arr.length; k++) {  
-      if (arr[j] == arr[k]) {
-        arr = arr.slice(j+1);
-        break;
-      }
-    }  
-
-    if (j == arr.length -1 && arr.length == 4) { 
-      console.log(i + 1);
-      isMarker = true;
-    }    
+      if (j == arr.length -1 && arr.length == 4) { 
+        console.log(i + 1);
+        isMarker = true;
+      }    
+    }
+    if (isMarker) break;
   }
-  if (isMarker) break;
 }
+
+function part2() {
+  let arr = [];
+  let isMarker = false; 
+  for (let i = 0; i< inputs.length; i++) {  
+    arr.push(inputs[i]);  
+    if (arr.length < 14) continue;
+    for (let j = 0; j< arr.length; j++) {
+      for (let k = j + 1; k< arr.length; k++) {  
+        if (arr[j] == arr[k]) {
+          arr = arr.slice(j+1);
+          break;
+        }
+      }  
+
+      if (j == arr.length -1 && arr.length == 14) { 
+        console.log(i + 1);
+        isMarker = true;
+      }    
+    }
+    if (isMarker) break;
+  }
+}
+
+//part1();
+part2();
 
 
 

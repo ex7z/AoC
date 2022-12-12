32b2804696abacda2001265da91320808a7183a8
ex7z <damian.kitov@gmail.com>
2022-12-12 16:36:41 +0200

    gg

--- 2022/day6.js
+++ 2022/day6.js
@@ -6,24 +6,21 @@ const inputs = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
 let arr = [];
 let isMarker = false; 
-for (let i = 0; i< inputs.length; i++) {
-  let isAgain = false; 
+for (let i = 0; i< inputs.length; i++) {  
   arr.push(inputs[i]);  
   if (arr.length < 4) continue;
   for (let j = 0; j< arr.length; j++) {
     for (let k = j + 1; k< arr.length; k++) {  
       if (arr[j] == arr[k]) {
-        arr = arr.slice(j +1);
-        isAgain = true;
+        arr = arr.slice(j+1);
         break;
       }
     }  
-    if (j == arr.length -1 && arr.length == 4 && !isAgain) { 
-      console.log(i + 1)
-      isMarker = true;
-    }
 
-    if (isAgain) continue;
+    if (j == arr.length -1 && arr.length == 4) { 
+      console.log(i + 1);
+      isMarker = true;
+    }    
   }
   if (isMarker) break;
 }
